2962b6f0759eed107055393eac0795b3f5ffaacb
Jim <j.vanderveen@gracious.nl>
2023-09-04 13:49:55 +0200

    F19-48 As a developer I want to be able to retrieve data based on channel so I can use proper dat...
    - Adding ability to fetch specific channel items from articles

--- src/resources/articles/index.ts
+++ src/resources/articles/index.ts
@@ -14,38 +14,69 @@ export default class Articles extends Base {
      * Get all articles for a project
      * @method getAllByProjectId
      * @param projectId
+     * @param channel
      * @returns {Promise<ArticlesResponse>}
      * @throws Error
      *
      */
-    async getAllByProjectId(projectId: string): Promise<ArticlesResponse> {
+    async getAllByProjectId(
+        projectId: string,
+        channel?: string
+    ): Promise<ArticlesResponse> {
         if (!projectId || projectId === "") {
             throw new Error("Project id not provided");
         }
 
         return this.request(`/cms/api/public/v1/article/project/${projectId}`, {
             method: "GET"
-        }).then(response => {
-            return response.json();
-        });
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then((data: ArticlesResponse) => {
+                if (channel && data.payload) {
+                    // If channel is provided, filter out articles without the specified channel
+                    data.payload = data.payload.filter(article => {
+                        return article.multiChannelTags.some(
+                            tag => tag.channel === channel
+                        );
+                    });
+                }
+                return data;
+            });
     }
 
     /**
      * Get an article by id
      * @method getById
      * @param articleId
+     * @param channel
      * @returns {Promise<ArticleResponse>}
      * @throws Error
      */
-    async getById(articleId: string): Promise<ArticleResponse> {
+    async getById(
+        articleId: string,
+        channel?: string
+    ): Promise<ArticleResponse> {
         if (!articleId || articleId === "") {
             throw new Error("Article id not provided");
         }
 
         return this.request(`/cms/api/public/v1/article/${articleId}`, {
             method: "GET"
-        }).then(response => {
-            return response.json();
-        });
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then((data: ArticleResponse) => {
+                if (channel && data.payload) {
+                    // If channel is provided, filter out articles without the specified channel
+                    data.payload.multiChannelTags =
+                        data.payload.multiChannelTags.filter(tag => {
+                            return tag.channel === channel;
+                        });
+                }
+                return data;
+            });
     }
 }

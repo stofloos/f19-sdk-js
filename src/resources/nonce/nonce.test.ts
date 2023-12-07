99d2e16f6a4ace9c45023987c1561df878c5cc98
Yuraymar Stewart <y.stewart@gracious.nl>
2023-12-07 17:06:58 +0100

    :bug: Fix nonce test

--- src/resources/nonce/nonce.test.ts
+++ src/resources/nonce/nonce.test.ts
@@ -1,13 +1,12 @@
 import "isomorphic-fetch";
 import { config } from "../../helpers/testing";
 import Client from "../../index";
+import Nonce from "./index";
 
 describe("Nonce resource", () => {
     const nonceResource = new Client(config).nonce;
 
-    it("should generate a nonce", async () => {
-        const nonce = await nonceResource.getNonce();
-        expect(nonce).toHaveProperty("payload");
-        expect(nonce.payload).toEqual(expect.any(String));
+    it("should be a nonce", async () => {
+        expect(nonceResource).toBeInstanceOf(Nonce);
     });
 });

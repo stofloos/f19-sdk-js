0f573b9801cb26bf71eae07e2111da38c6b95059
Yuraymar Stewart <y.stewart@gracious.nl>
2024-02-02 09:38:34 +0100

    :zap: Refresh token after 55 minutes

--- src/resources/base.ts
+++ src/resources/base.ts
@@ -65,8 +65,8 @@ export default abstract class Base {
                 method,
                 options
             );
-            // Current Time + 59 minutes
-            const expires = Date.now() + 3540000;
+            // Current Time + 55 minutes
+            const expires = Date.now() +  55 * 60 * 1000;
 
             // Check if requestToken is already cached and remove it
             if (this.requestTokens.has(uri)) {

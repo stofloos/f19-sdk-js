2f5b8efe12da3c137cbd2a489e8b9c623c87d731
Yuraymar Stewart <y.stewart@gracious.nl>
2024-02-09 14:40:13 +0100

    :fire: Remove update token if token exsists

--- src/helpers/cache.ts
+++ src/helpers/cache.ts
@@ -1,5 +1,5 @@
 export default class TimeBasedCache {
-    private cache = {} as {
+    private readonly cache = {} as {
         [key: string]: { value: string; expirationTime: number };
     };
     constructor() {
@@ -7,16 +7,10 @@ export default class TimeBasedCache {
     }
 
     set(key: string | number, value: string, expirationTimeMs: number) {
-        // Check if the key already exists in the cache
-
-        // eslint-disable-next-line no-console
-        console.log("Cache.Set", key, expirationTimeMs, Date.now());
-
-        if (this.cache[key]) {
-            // Update the existing entry
-            this.update(key, value, expirationTimeMs);
-        } else {
-            // Add a new entry if the key doesn't exist
+        // Add a new entry if the key doesn't exist
+        if (!this.cache[key]) {
+            // eslint-disable-next-line no-console
+            console.log("Cache.Set", key, expirationTimeMs, Date.now());
             this.cache[key] = {
                 value,
                 expirationTime: Date.now() + expirationTimeMs
@@ -27,22 +21,6 @@ export default class TimeBasedCache {
                 this.remove(key);
             }, expirationTimeMs);
         }
-    }
-
-    update(key: string | number, value: string, expirationTimeMs: number) {
-        // eslint-disable-next-line no-console
-        console.log("Cache.Update", key, expirationTimeMs, Date.now());
-
-        // Update the value and expiration time for the existing entry
-        this.cache[key] = {
-            value,
-            expirationTime: Date.now() + expirationTimeMs
-        };
-
-        // Reschedule the timeout for the updated entry
-        setTimeout(() => {
-            this.remove(key);
-        }, expirationTimeMs);
     }
 
     get(key: string | number) {
@@ -60,9 +38,5 @@ export default class TimeBasedCache {
         // eslint-disable-next-line no-console
         console.log("Cache.Remove", key, Date.now());
         delete this.cache[key];
-    }
-
-    clear() {
-        this.cache = {};
     }
 }

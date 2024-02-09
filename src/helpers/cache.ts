da8826bbe6aecd7233b6cb32764a91ebafa7dd83
Yuraymar Stewart <y.stewart@gracious.nl>
2024-02-09 16:03:20 +0100

    :zap: Improve caching stategey

--- src/helpers/cache.ts
+++ src/helpers/cache.ts
@@ -1,25 +1,34 @@
+type TimeBasedCacheEntry = {
+    value: string;
+    expirationTime: number;
+    timeoutId: NodeJS.Timeout;
+};
+
+type TimeBasedCacheType = {
+    [key: string]: TimeBasedCacheEntry;
+};
+
 export default class TimeBasedCache {
-    private readonly cache = {} as {
-        [key: string]: { value: string; expirationTime: number };
-    };
+    private cache: TimeBasedCacheType;
     constructor() {
         this.cache = {};
     }
 
     set(key: string | number, value: string, expirationTimeMs: number) {
         // Add a new entry if the key doesn't exist
-        if (!this.cache[key]) {
+        if (this.cache[key]) {
+            // Update the existing entry
+            this.update(key, value, expirationTimeMs);
+        } else {
             // eslint-disable-next-line no-console
             console.log("Cache.Set", key, expirationTimeMs, Date.now());
             this.cache[key] = {
                 value,
-                expirationTime: Date.now() + expirationTimeMs
+                expirationTime: Date.now() + expirationTimeMs,
+                timeoutId: setTimeout(() => {
+                    this.remove(key);
+                }, expirationTimeMs)
             };
-
-            // Schedule a timeout to remove the entry after the specified expiration time
-            setTimeout(() => {
-                this.remove(key);
-            }, expirationTimeMs);
         }
     }
 
@@ -37,6 +46,44 @@ export default class TimeBasedCache {
     remove(key: string | number) {
         // eslint-disable-next-line no-console
         console.log("Cache.Remove", key, Date.now());
+
+        const timeoutId = this.cache[key]?.timeoutId;
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
         delete this.cache[key];
+    }
+
+    update(key: string | number, value: string, expirationTimeMs: number) {
+        // eslint-disable-next-line no-console
+        console.log("Cache.Update", key, expirationTimeMs, Date.now());
+
+        // Clear the existing timeout
+        const existingTimeoutId = this.cache[key]?.timeoutId;
+        if (existingTimeoutId) {
+            clearTimeout(existingTimeoutId);
+        }
+
+        // Update the value and expiration time for the existing entry
+        this.cache[key] = {
+            value,
+            expirationTime: Date.now() + expirationTimeMs,
+            timeoutId: setTimeout(() => {
+                this.remove(key);
+            }, expirationTimeMs)
+        };
+    }
+
+    clear(): void {
+        // Clear all timeouts and reset the cache
+        Object.keys(this.cache).forEach(key => {
+            const timeoutId = this.cache[key].timeoutId;
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+        });
+
+        this.cache = {};
     }
 }
